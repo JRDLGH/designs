d5114821c2d4141bd519b9f937fa8a152df94eff
JRDLGH <ipes.lagache@gmail.com>
2018-03-24 15:11:52 +0100

    Updating Mobile menu

--- TFE/js/menu.js
+++ TFE/js/menu.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
     $('.menu-icon').click(function(evt){
         evt.preventDefault();
-        //if already open
-        if($(this).hasClass('opened')){
-            //close
-            $(this).removeClass('opened');
-            $('.nav-links').hide();
-        }else{
-            //open
-            $(this).addClass('opened');
-            console.log('hello');
-            $('.nav-links').show();
+        //if screens are smaller than x px
+        if($(window).width() < 1024 ){
+            //if already open
+            if($(this).hasClass('opened')){
+                //close
+                $(this).removeClass('opened');
+                $('.nav-links').animate({
+                    duration: 1000,
+                    height: 'toggle',
+                    easing: 'swing'
+                });
+            }else{
+                //open
+                $(this).addClass('opened');
+                $('.nav-links').animate({
+                    duration: 1000,
+                    height: 'toggle',
+                    easing: 'swing'
+                });
+            }
+        }
+    });
+    $(window).resize(function(){
+        if($(this).width() >= 1024)
+        {
+            if($('.nav-links').css('display') == 'none' || $('.nav-links').css('display') == 'block'){
+                $('.nav-links').removeAttr('style');
+                $('.menu-icon').removeClass('opened');
+            }
         }
     });
 });
